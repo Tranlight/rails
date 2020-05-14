00f5a7276f5de9a45ad50fd50c6b1ec32a48e92e
tranlight <tranlight98@gmail.com>
2020-05-14 14:50:00 +0700

    Cau 1 -12

---
+++ app/models/line_item.rb
@@ -0,0 +1,9 @@
+class LineItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :order, optional: true
+  belongs_to :product, optional: true
+
+  def total_price
+    product.price * quantity
+  end
+end
