a266c6630d4a2a651df096cb1cebeb57de35f913
tranlight <tranlight98@gmail.com>
2020-05-15 13:05:37 +0700

    Internationalization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,8 +34,8 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
-        format.html { redirect_to store_index_url, notice: "Thank you for your order." }
+        # OrderMailer.received(@order).deliver_later
+        format.html { redirect_to store_index_url, I18n.t(".thanks") }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
